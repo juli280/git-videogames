793bc27fd21cd94e4624b120c19fee4204a85715
Julieta Cecilia <julicecilia@192.168.1.9>
2026-09-16 23:24:09 -0300

    Add updateCounter functionality

--- src/main.ts
+++ src/main.ts
@@ -30,9 +30,10 @@ function renderCards(games: Game[]): void {
 
 function updateCounter(_count: number): void {
   const el = document.querySelector<HTMLSpanElement>('#count')!
+
   // TODO: mostrar el número de juegos en el elemento #count.
   // Asigná el valor a el.textContent como string.
-  el.textContent = ''
+  el.textContent = String(el)
 }
 
 function getGenres(): string[] {
